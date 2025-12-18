b32fc29ad23b7484bae989977e274feccab23973
vietanhrs <vietanhtran.uet@gmail.com>
2025-12-18 15:30:55 +0700

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,151 @@
+<script>
+</script>
+
+<ol class="px-10 py-2">
+	<li>
+		<div class="d-flex w-100 justify-content-between">
+			<h5>Co-founder</h5>
+			<span>November 2025 - now</span>
+		</div>
+		<ul>
+			<li>
+				<p>
+					Wins Residence @<a href="https://www.winsresidence.com/" target="_blank"
+						>winsresidence.com</a
+					>
+				</p>
+			</li>
+			<li>
+				<p>Venture Capital, Digital Marketing, Digital Media, Management, Foreign Languages</p>
+			</li>
+		</ul>
+	</li>
+	<li>
+		<div class="d-flex w-100 justify-content-between">
+			<h5>Blockchain & Software Engineer</h5>
+			<span>November 2022 - now</span>
+		</div>
+		<ul>
+			<li>
+				<p>Teko Solution</p>
+			</li>
+			<li>
+				<p>Micro-frontend, Aiken, Golang, Kotlin, React, CI/CD, JavaScript/TypeScript</p>
+			</li>
+		</ul>
+	</li>
+	<li>
+		<div class="d-flex w-100 justify-content-between">
+			<h5>Data mining and analysis in Decentralized Finance</h5>
+			<span>April 2022 - now</span>
+		</div>
+		<ul>
+			<li>
+				<p>Client: Faculty of Finance & Banking - UEB - VNU</p>
+			</li>
+			<li>
+				<p>Python, GraphQL</p>
+			</li>
+			<li>
+				<p>
+					Result: <a href="https://www.sciencedirect.com/science/article/pii/S1044028324001273"
+						>The determinants of funding liquidity risk in decentralized lending</a
+					>
+				</p>
+			</li>
+		</ul>
+	</li>
+	<li>
+		<div class="d-flex w-100 justify-content-between">
+			<h5>Developing decentralized applications</h5>
+			<span>October 2021 - June 2022</span>
+		</div>
+		<ul>
+			<li>
+				<p>Company: TVM Digital</p>
+			</li>
+			<li>
+				<p>Angular, Node.js, Solidity, Ethereum</p>
+			</li>
+		</ul>
+	</li>
+	<li>
+		<div class="d-flex w-100 justify-content-between">
+			<h5>Blockchain Research</h5>
+			<span>September 2021 - November 2022</span>
+		</div>
+		<ul>
+			<li>
+				<p>Blockchain Research and Application Laboratory - UET</p>
+			</li>
+			<li>
+				<p>Supervisor: Associate Professor PhD. Trương Anh Hoàng.</p>
+			</li>
+			<li>
+				<p>Solidity, Rust, Ethereum, Avalanche, Terra</p>
+			</li>
+		</ul>
+	</li>
+	<li>
+		<div class="d-flex w-100 justify-content-between">
+			<h5>Teaching Assistant</h5>
+			<span>2020 - 2022</span>
+		</div>
+		<ul>
+			<li>
+				<p>Class: Robotics (INT 3409) - ROS (Robot Operating System)</p>
+			</li>
+			<li>
+				<p>Teachers: PhD. Ta Viet Cuong, PhD. Le Nguyen Khoi, Grad Student. Pham Dình Tuan.</p>
+			</li>
+			<li>
+				<p>C++, Python, Tensorflow, OpenCV</p>
+			</li>
+		</ul>
+	</li>
+	<li>
+		<div class="d-flex w-100 justify-content-between">
+			<h5>Evolutionary Computation Research for Deep Learning</h5>
+			<span>2020 - 2022</span>
+		</div>
+		<ul>
+			<li>
+				<p>Computer Science Laboratory - UET</p>
+			</li>
+			<li>
+				<p>Supervisor: PhD. Le Nguyen Khoi.</p>
+			</li>
+			<li>
+				<p>Python, Tensorflow</p>
+			</li>
+		</ul>
+	</li>
+	<li>
+		<div class="d-flex w-100 justify-content-between">
+			<h5>Machine Learning/Computer Vision Research for Robotics</h5>
+			<span>2019 - 2022</span>
+		</div>
+		<ul>
+			<li>
+				<p>Computer Science Laboratory - UET</p>
+			</li>
+			<li>
+				<p>Supervisor: PhD. Le Nguyen Khoi.</p>
+			</li>
+			<li>
+				<p>C++, Python, Tensorflow, OpenCV</p>
+			</li>
+		</ul>
+	</li>
+	<li>
+		<div class="d-flex w-100 justify-content-between">
+			<h5>Ho Thi Hang's Boyfriend</h5>
+			<span>June 2023 - now</span>
+		</div>
+		<ul>
+			<li>
+				<p>Cooking</p>
+			</li>
+		</ul>
+	</li>
+</ol>
