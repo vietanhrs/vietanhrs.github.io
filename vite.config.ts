f56850fe4d2bf69c9c283c7eab1b0031355b2409
vietanhrs <vietanhtran.uet@gmail.com>
2025-12-22 09:12:57 +0700

    Update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,7 @@ export default defineConfig(({ mode }) => ({
     port: 8080,
   },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+  base: process.env.NODE_ENV === 'production' ? '/vietanhrs.github.io/' : '/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
